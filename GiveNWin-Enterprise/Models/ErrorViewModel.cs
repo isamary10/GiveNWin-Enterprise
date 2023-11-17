dd03067f13d6b11f0fa477693134db2bbfa73ca9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-16 21:00:37 -0300

    Criação do projeto

---
+++ GiveNWin-Enterprise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GiveNWin_Enterprise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
